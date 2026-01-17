be336da5d37424dbb7bc36033b6e851c10776458
Maai Ham <51383278+mahamtr@users.noreply.github.com>
2026-01-17 19:18:01 +0100

    adding new frontend with tamagui and nestjs with supacase guard

---
+++ src/frontend/src/lib/supacase.ts
@@ -0,0 +1,13 @@
+import 'expo-sqlite/localStorage/install'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: false, // important for native
+  },
+})
